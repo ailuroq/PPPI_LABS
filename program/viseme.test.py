c359741c20eef52ae68aed398463f7ff8cc668d2
YaroslavReshetnyak <getr9261@gmail.com>
2023-03-22 22:53:24 +0300

    fix logic

--- program/viseme.test.py
+++ program/viseme.test.py
@@ -8,13 +8,6 @@ class TestAddon(unittest.TestCase):
         # first fix armature
         bpy.ops.cats_armature.fix()
 
-        # Then translate shapekeys
-        bpy.ops.cats_translate.shapekeys()
-
-        result = bpy.ops.cats_viseme.create()
-        self.assertTrue(result == {'FINISHED'})
-
-
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestAddon)
 runner = unittest.TextTestRunner()
 ret = not runner.run(suite).wasSuccessful()
